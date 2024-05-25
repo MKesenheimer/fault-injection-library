dd08213390a98f6511f4aa97be344eb06c257cb3
MKesenheimer <m.kesenheimer@gmx.net>
2024-05-25 10:37:39 +0200

    Power cycle bugfixing

--- stm32f4-glitching/power-cycle.py
+++ stm32f4-glitching/power-cycle.py
@@ -4,14 +4,11 @@ import logging
 import random
 import sys
 import time
-import serial
 
 # import custom libraries
 sys.path.insert(0, "../lib/")
-from FaultInjectionLib import Database, ProGlitcher
+from FaultInjectionLib import ProGlitcher
 
 glitcher = ProGlitcher()
-ser = serial.Serial(port="/dev/tty.usbmodem11402", baudrate=115200, timeout=0.25, bytesize=8, parity='E', stopbits=1)
 glitcher.init()
-
 glitcher.power_cycle_target()
