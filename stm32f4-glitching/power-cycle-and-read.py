579bde8f6de4c8059dadd89423910c7534bb8eba
MKesenheimer <m.kesenheimer@gmx.net>
2024-05-28 20:15:23 +0200

    power-cycle-and-read script fixed

--- stm32f4-glitching/power-cycle-and-read.py
+++ stm32f4-glitching/power-cycle-and-read.py
@@ -2,36 +2,43 @@
 import sys
 import time
 
+
 # import custom libraries
 sys.path.insert(0, "../lib/")
 from BootloaderCom import BootloaderCom
 from FaultInjectionLib import ProGlitcher
 
-glitcher = ProGlitcher()
-glitcher.init()
-bootcom = BootloaderCom(port="/dev/ttyUSB0")
+def main():
+    glitcher = ProGlitcher()
+    glitcher.init()
+    bootcom = BootloaderCom(port="/dev/ttyUSB0")
+    mem = b""
+    while True:
+        # reset target
+        #glitcher.reset(0.01)
+        glitcher.power_cycle_target()
+        time.sleep(0.5)
 
-while True:
-    # reset target
-    #glitcher.reset(0.01)
-    glitcher.power_cycle_target()
-    time.sleep(0.5)
+        # setup bootloader communication
+        response = bootcom.init_get_id()
+        print(response)
 
-    # setup bootloader communication
-    response = bootcom.init_get_id()
-    print(response)
-    if response != 0:
+        if response == 0:
+            response = bootcom.setup_memread()
+            print(response)
+
+        if response == 0:
+            start = 0x08000000
+            size  = 0xff
+            response, mem = bootcom.read_memory(start, size)
+            print(response)
+
+        if response == 0:
+            print(mem)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExitting...")
         sys.exit(1)
-
-    response = bootcom.setup_memread()
-    print(response)
-    if response != 0:
-        sys.exit(1)
-
-    start = 0x08000000
-    size  = 0xff
-    response, mem = bootcom.read_memory(start, size)
-    print(response)
-    if response == 0:
-        print(mem)
-    sys.exit(1)
