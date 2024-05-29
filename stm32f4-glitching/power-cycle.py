4b6f1cd6169129ae6aef7a670cfb6fac8adfd453
MKesenheimer <m.kesenheimer@gmx.net>
2024-05-29 12:45:55 +0200

    better output

--- stm32f4-glitching/power-cycle.py
+++ stm32f4-glitching/power-cycle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-import argparse
-import logging
-import random
 import sys
-import time
 
 # import custom libraries
 sys.path.insert(0, "../lib/")
 from FaultInjectionLib import ProGlitcher
 
+print("[+] Initializing the ProGlitcher")
 glitcher = ProGlitcher()
 glitcher.init()
-glitcher.power_cycle_target()
+print("[+] Power cycling target.")
+glitcher.power_cycle_target(1)
+print("[+] Done.")
