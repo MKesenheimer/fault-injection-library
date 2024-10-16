6fdfbc381b41d221f683ca403279c410321525be
MKesenheimer <m.kesenheimer@gmx.net>
2024-10-16 19:33:40 +0200

    Added example script to test the pico-glitcher

--- example/pico-glitcher.py
+++ example/pico-glitcher.py
@@ -5,10 +5,12 @@
 # You should have received a copy of the GPL3 license with this file.
 # If not, please write to: m.kesenheimer@gmx.net.
 
-# SQL Queries:
-# Show only successes and flash-resets:
-# color = 'R' or response LIKE '_Warning.flash_reset'
-
+# This script can be used to test the pico-glitcher.
+# -> Connect Trigger input with Reset.
+# -> Connect Glitch with VTarget.
+# -> Run the script:
+# python pico-glitcher-test.py --rpico /dev/tty.usbmodem1101 --delay 100 100 --length 100 100
+# -> Observe the glitches with a oscilloscope.
 
 import argparse
 import logging
@@ -17,19 +19,21 @@ import sys
 import time
 
 # import custom libraries
-from findus import Database, PicoGlitcher, Serial
+from findus import Database, PicoGlitcher
 
 # inherit functionality and overwrite some functions
 class DerivedGlitcher(PicoGlitcher):
-    def classify(self, expected, response):
-        if response == expected:
+    def classify(self, response):
+        if b'Trigger ok' in response:
             color = 'G'
-        elif b'Falling' in response:
-            color = 'R'
+        elif b'Error' in response:
+            color = 'M'
         elif b'Fatal exception' in response:
             color = 'M'
-        else:
+        elif b'Timeout' in response:
             color = 'Y'
+        else:
+            color = 'R'
         return color
 
 class Main():
@@ -43,19 +47,14 @@ class Main():
         self.glitcher = DerivedGlitcher()
         # if argument args.power is not provided, the internal power-cycling capabilities of the pico-glitcher will be used. In this case, ext_power_voltage is not used.
         self.glitcher.init(port=args.rpico, ext_power=args.power, ext_power_voltage=3.3)
-        # choose rising edge trigger with dead time of 0.03 seconds after power down
-        # note that you still have to physically connect the trigger input with the reset line
-        self.glitcher.rising_edge_trigger(0.005, "reset")
+        # choose rising edge trigger with dead time of 0 seconds after power down
+        # note that you still have to physically connect the trigger input with vtarget
+        self.glitcher.rising_edge_trigger(0, "reset")
         # choose crowbar transistor
         self.glitcher.set_lpglitch()
 
         # set up the database
         self.database = Database(sys.argv, resume=self.args.resume, nostore=self.args.no_store)
-
-        # set up serial communication with the device under test
-        self.target = Serial(port=self.args.target, timeout=0.1)
-        self.target.init()
-
         self.start_time = int(time.time())
 
     def run(self):
@@ -67,29 +66,26 @@ class Main():
         s_delay = self.args.delay[0]
         e_delay = self.args.delay[1]
 
-        expected = b'ets Jun  8 2016 00:22:57\r\n\r\nrst:0x1 (POWERON_RESET),boot:0x13 (SPI_FAST_FLASH_BOOT)\r\nconfigsip: 0, SPIWP:0xee\r\nclk_drv:0x00,q_drv:0x00,d_drv:0x00,cs0_drv:0x00,hd_drv:0x00,wp_drv:0x00\r\nmode:DOUT, clock div:2\r\nload:0x40080400,len:16384\r\ncsum err:0xef!=0xff\r\nets_main.c 371 \r\n'
-
         experiment_id = 0
         while True:
-            # empty rx buffer
-            self.target.empty_read_buffer()
-
             # set up glitch parameters (in nano seconds) and arm glitcher
             length = random.randint(s_length, e_length)
             delay = random.randint(s_delay, e_delay)
             self.glitcher.arm(delay, length)
 
             # power cycle target
-            #self.glitcher.power_cycle_target(0.03)
+            #self.glitcher.power_cycle_target(0.1)
 
             # reset target
+            time.sleep(0.01)
             self.glitcher.reset(0.01)
 
             # block until glitch
             try:
-                self.glitcher.block(timeout=1)
-                # send command and read response
-                response = self.target.read(len(expected))
+                self.glitcher.block(timeout=2)
+                # Manually set the response to a reasonable value.
+                # In a real scenario, this would be filled by the response of the microcontroller (UART, SWD, etc.)
+                response = b'Trigger ok'
             except Exception as _:
                 print("[-] Timeout received in block(). Continuing.")
                 self.glitcher.power_cycle_target(power_cycle_time=1)
@@ -97,7 +93,7 @@ class Main():
                 response = b'Timeout'
 
             # classify response
-            color = self.glitcher.classify(expected, response)
+            color = self.glitcher.classify(response)
 
             # add to database
             self.database.insert(experiment_id, delay, length, color, response)
@@ -110,14 +106,9 @@ class Main():
             # increase experiment id
             experiment_id += 1
 
-            # Dump finished
-            #if color == 'R':
-            #    break
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--rpico", required=False, help="rpico port", default="/dev/ttyACM0")
-    parser.add_argument("--target", required=False, help="target port", default="/dev/ttyUSB1")
     parser.add_argument("--power", required=False, help="rk6006 port", default=None)
     parser.add_argument("--delay", required=True, nargs=2, help="delay start and end", type=int)
     parser.add_argument("--length", required=True, nargs=2, help="length start and end", type=int)
